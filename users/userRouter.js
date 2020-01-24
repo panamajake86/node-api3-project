f9f53f4f1ae5cd88f88a6d6e7175b85f8ee942c7
Jake <panamajake86@gmail.com>
2020-01-23 21:06:05 -0700

    getting closer

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const User = require('./userDb');
+const Post = require('../posts/postDb');
 
 const router = express.Router();
 
 router.post('/', validateUser, (req, res) => {
   // do your magic!
-  res.status(200).json(req.name)
+
+  User.insert(req.body)
+    .then(user => {
+        res.status(201).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({ message: 'Server error', err });
+    })
 });
 
-router.post('/:id/posts', validatePost, validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // do your magic!
+  const { id } = req.params;
+  const body = req.body;
+  const userId = body.user_id;
+
+  if (userId == id) {
+    Post.insert(body)
+      .then(post => {
+        res.status(201).json(post);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Server error, try again later', err});
+      })
+  }
+
+
 });
 
 router.get('/', (req, res) => {
@@ -25,8 +48,21 @@ router.get('/', (req, res) => {
 
 router.get('/:id', validateUserId, (req, res) => {
   // do your magic!
-  res.status(200).json(req.params);
-});
+  const { id } = req.params;
+
+  User.getById(id)
+    .then(users => {
+  if (users) {
+      res.status(200).json(users);
+    } else {
+      res.status(400).json({ message: "invalid user id" });
+    }
+   
+  })
+    .catch(err => {
+      res.status(500).json({ message: "exception", err })
+    });
+  });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
   // do your magic!
@@ -48,8 +84,8 @@ function validateUserId(req, res, next) {
 
   User.getById(id)
     .then(users => {
-      if(users) {
-        req.id = id;
+      if (users) {
+        req.id = users.id;
         next();
       } else {
         res.status(400).json({ message: "invalid user id" });
@@ -62,24 +98,36 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
   // do your magic!
-  const body = req.name;
+  const body = req.body;
+  const { name } = body;
 
-  User.get(body)
-    .then(users => {
-      if(users) {
-        req.name = name;
-        next();
-      } else {
-        res.status(400).json({ message: "invalid user id" });
-      }
-    })
-    .catch(err => {
-      res.status(500).json({ message: "exception", err });
-    });
+  if (!body) {
+    res.status(400).json({ message: 'missing body of user' });
+  } 
+  if (!name) {
+    res.status(400).json({ message: 'missing required name field' });
+  }
+
+  next();
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  const body = req.body;
+  const { name }  = body;
+  const { user_id} = body;
+
+  if (!cody) {
+    res.status(400).json({ message: 'missing body of post' });
+  }
+  if (!name) {
+    res.status(400).json({ message: 'missing required name field' });
+  }
+  if (!user_id) {
+    res.status(400).json({ message: 'missing required name field' });
+  }
+
+  next()
 }
 
 module.exports = router;
